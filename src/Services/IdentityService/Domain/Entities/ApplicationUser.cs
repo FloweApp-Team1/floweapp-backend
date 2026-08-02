dd5997592208a38e08709feaee1183403f6b7caa
Ahmad El Gar7y <ahmadelgary@gmail.com>
2026-08-02 19:11:25 +0300

    identityService/Domains/create-models-without-Migration

---
+++ src/Services/IdentityService/Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using IdentityService.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.Domain.Entities
+{
+    public class ApplicationUser:IdentityUser<Guid>
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public GenderEnum Gender { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+
+    }
+}
---
+++ src/Services/IdentityService/Domain/Entities/VehicleInfo.cs
@@ -0,0 +1,14 @@
+﻿using IdentityService.Domain.Enums;
+
+namespace IdentityService.Domain.Entities
+{
+    public class VehicleInfo
+    {
+        public Guid Id { get; set; } 
+        public VehicleTypeEnum Type { get; set; }
+        public string PlateNumber { get; set; } = null!;
+        public string Capacity { get; set; } = null!;
+
+
+    }
+}
