45dc4785c6a95766cc6e656dde611bb1b32822fe
rahma2005 <rhmarhma360@gmail.com>
2026-08-11 20:03:47 +0300

    add list gategories endpoint

--- src/Services/CatalogService/Features/Categories/ListCategories/ListCategoriesEndpoint.cs
+++ src/Services/CatalogService/Features/Categories/ListCategories/ListCategoriesEndpoint.cs
@@ -1,4 +1,7 @@
+using CatalogService.Features.Categories.ListCategories.Dtos;
+using MediatR;
 using Shared.Contracts;
+using Shared.Requests;
 using Shared.Responses;
 
 namespace CatalogService.Features.Categories.ListCategories;
@@ -7,10 +10,17 @@ public class ListCategoriesEndpoint : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("/categories", () =>
-                ApiResponse.Success(new { }, "Categories retrieved").ToHttpResult())
+        app.MapGet("/categories", async (
+                [AsParameters] PaginationRequest pagination,
+                ISender sender,
+                CancellationToken cancellationToken) =>
+            {
+                var result = await sender.Send(new ListCategoriesQuery(pagination), cancellationToken);
+                return result.ToHttpResult();
+            })
             .WithTags("Categories")
             .WithName("ListCategories")
+            .Produces<ApiResponse<IReadOnlyList<ListCategoriesResponse>>>(StatusCodes.Status200OK)
             .AllowAnonymous();
     }
 }
