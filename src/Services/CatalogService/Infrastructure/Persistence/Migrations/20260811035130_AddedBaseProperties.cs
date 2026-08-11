f788e9b90f5524f3fcf553c4601fb6d3977c1159
Shahd <shahd.mostafa12@gmail.com>
2026-08-11 07:05:02 +0300

    Add base properties to Catalog schema tables Migration

---
+++ src/Services/CatalogService/Infrastructure/Persistence/Migrations/20260811035130_AddedBaseProperties.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogService.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBaseProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastChangedBy",
+                schema: "Catalog",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "Catalog",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                schema: "Catalog",
+                table: "ProductImages",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastChangedBy",
+                schema: "Catalog",
+                table: "ProductImages",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "Catalog",
+                table: "ProductImages",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastChangedBy",
+                schema: "Catalog",
+                table: "Occasions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "Catalog",
+                table: "Occasions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastChangedBy",
+                schema: "Catalog",
+                table: "Categories",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "Catalog",
+                table: "Categories",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastChangedBy",
+                schema: "Catalog",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                schema: "Catalog",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                schema: "Catalog",
+                table: "ProductImages");
+
+            migrationBuilder.DropColumn(
+                name: "LastChangedBy",
+                schema: "Catalog",
+                table: "ProductImages");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                schema: "Catalog",
+                table: "ProductImages");
+
+            migrationBuilder.DropColumn(
+                name: "LastChangedBy",
+                schema: "Catalog",
+                table: "Occasions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                schema: "Catalog",
+                table: "Occasions");
+
+            migrationBuilder.DropColumn(
+                name: "LastChangedBy",
+                schema: "Catalog",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                schema: "Catalog",
+                table: "Categories");
+        }
+    }
+}
