ec7c9d2cd5c24dccff1749dcc1d938be3ad4963a
Sara Maged <Sara Maged@DESKTOP-JJMISKB>
2026-08-15 14:17:22 +0300

    add migration AddProductSortAndSearchIndexes

---
+++ src/Services/CatalogService/Infrastructure/Persistence/Migrations/20260815111628_AddProductSortAndSearchIndexes.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogService.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductSortAndSearchIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_products_createdat_id",
+                schema: "Catalog",
+                table: "Products",
+                columns: new[] { "CreatedAt", "Id" });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_products_price_id",
+                schema: "Catalog",
+                table: "Products",
+                columns: new[] { "Price", "Id" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_products_createdat_id",
+                schema: "Catalog",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "ix_products_price_id",
+                schema: "Catalog",
+                table: "Products");
+        }
+    }
+}
