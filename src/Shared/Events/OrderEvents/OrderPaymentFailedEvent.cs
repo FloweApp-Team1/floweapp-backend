befe08afc856ea0910ee31b636569094d56f8d82
Ahmed Osama <141922572+AhmedUsama29@users.noreply.github.com>
2026-08-22 22:04:57 +0300

    deleted unused events

--- src/Shared/Events/OrderEvents/OrderPaymentFailedEvent.cs
+++
@@ -1,8 +0,0 @@
-namespace Shared.Events.OrderEvents
-{
-    public record OrderPaymentFailedEvent
-    {
-        public Guid OrderId { get; init; }
-        public Guid PaymentAttemptId { get; init; }
-    }
-}
--- src/Shared/Events/OrderEvents/OrderPaymentSucceededEvent.cs
+++
@@ -1,8 +0,0 @@
-namespace Shared.Events.OrderEvents
-{
-    public record OrderPaymentSucceededEvent
-    {
-        public Guid OrderId { get; init; }
-        public Guid PaymentAttemptId { get; init; }
-    }
-}
