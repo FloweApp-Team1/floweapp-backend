708ad2d2296aa48697c4f7b9fa1cfe3d8d046670
Sara Maged <saramaged660@gmail.com>
2026-08-27 04:41:08 +0300

    update PlaceOrderValidator rules

--- src/Services/OrdersService/Features/Checkout/PlaceOrder/PlaceOrderValidator.cs
+++ src/Services/OrdersService/Features/Checkout/PlaceOrder/PlaceOrderValidator.cs
@@ -1,53 +1,39 @@
 ﻿using FluentValidation;
-
+using OrdersService.Domain.Enums;
 namespace OrdersService.Features.Checkout.PlaceOrder
 {
-    
-        public sealed class PlaceOrderValidator : AbstractValidator<PlaceOrderCommand>
+    public sealed class PlaceOrderValidator : AbstractValidator<PlaceOrderCommand>
+    {
+        public PlaceOrderValidator()
         {
-            public PlaceOrderValidator()
+            RuleFor(x => x.CartId).NotEmpty();
+            RuleFor(x => x.AddressId).NotEmpty();
+            RuleFor(x => x.IdempotencyKey).NotEmpty();
+            RuleFor(x => x.PaymentMethod).IsInEnum();
+
+            When(x => x.PaymentMethod == PaymentMethodEnum.Card, () =>
             {
-                RuleFor(x => x.CartId).NotEmpty();
+                RuleFor(x => x.PaymentGateway)
+                    .NotEmpty()
+                    .WithMessage("paymentGateway is required when paymentMethod is Card.");
+            });
 
-                RuleFor(x => x)
-                    .Must(x => x.AddressId.HasValue ^ x.NewAddress is not null)
-                    .WithMessage("Provide either addressId or newAddress, not both or neither.");
+            When(x => x.IsGift, () =>
+            {
+                RuleFor(x => x.GiftRecipient)
+                    .NotNull()
+                    .WithMessage("Gift recipient details are required when sending as a gift.");
 
-                RuleFor(x => x.PaymentMethod).IsInEnum();
-
-                When(x => x.NewAddress is not null, () =>
+                When(x => x.GiftRecipient is not null, () =>
                 {
-                    RuleFor(x => x.NewAddress!.RecipientName).NotEmpty().MaximumLength(150);
+                    RuleFor(x => x.GiftRecipient!.RecipientName).NotEmpty().MaximumLength(150);
 
-                    RuleFor(x => x.NewAddress!.RecipientPhone)
+                    RuleFor(x => x.GiftRecipient!.RecipientPhone)
                         .NotEmpty()
                         .Matches(@"^01[0125][0-9]{8}$")
                         .WithMessage("Phone must be a valid Egyptian mobile number.");
-
-                    RuleFor(x => x.NewAddress!.AddressLine).NotEmpty().MaximumLength(500);
-                    RuleFor(x => x.NewAddress!.City).NotEmpty().MaximumLength(100);
-                    RuleFor(x => x.NewAddress!.Area).NotEmpty().MaximumLength(100);
                 });
-
-                
-                When(x => x.IsGift, () =>
-                {
-                    RuleFor(x => x.GiftRecipient)
-                        .NotNull()
-                        .WithMessage("Gift recipient details are required when sending as a gift.");
-
-                    When(x => x.GiftRecipient is not null, () =>
-                    {
-                        RuleFor(x => x.GiftRecipient!.RecipientName).NotEmpty().MaximumLength(150);
-
-                        RuleFor(x => x.GiftRecipient!.RecipientPhone)
-                            .NotEmpty()
-                            .Matches(@"^01[0125][0-9]{8}$")
-                            .WithMessage("Phone must be a valid Egyptian mobile number.");
-
-                        RuleFor(x => x.GiftRecipient!.RecipientAddress).NotEmpty().MaximumLength(500);
-                    });
-                });
-            }
+            });
         }
+    }
 }
