7c3ef3cca3a5c69c7a58ac33837c9cc91cdfd375
Sara Maged <saramaged660@gmail.com>
2026-08-29 00:56:23 +0300

    refactor(address-cart): convert StoreAndAddressSeeder to seed multi-coverage stores only

--- src/Services/AddressCartService/Infrastructure/Persistence/Seed/SeedIds.cs
+++ src/Services/AddressCartService/Infrastructure/Persistence/Seed/SeedIds.cs
@@ -21,15 +21,6 @@
             public static readonly Guid GizaZayedRadius = Guid.Parse("88888888-8888-8888-8888-888888888888");
         }
 
-        public static class Addresses
-        {
-            public static readonly Guid Maadi = Guid.Parse("bbbbbbbb-0000-0000-0000-000000000001");
-            public static readonly Guid NasrCity = Guid.Parse("bbbbbbbb-0000-0000-0000-000000000002");
-            public static readonly Guid Zamalek = Guid.Parse("bbbbbbbb-0000-0000-0000-000000000003");
-            public static readonly Guid Heliopolis = Guid.Parse("bbbbbbbb-0000-0000-0000-000000000004");
-            public static readonly Guid Tagamoa = Guid.Parse("bbbbbbbb-0000-0000-0000-000000000005");
-            public static readonly Guid Alexandria = Guid.Parse("bbbbbbbb-0000-0000-0000-000000000006");
-            public static readonly Guid Zayed = Guid.Parse("bbbbbbbb-0000-0000-0000-000000000007");
-        }
+        
     }
 }
