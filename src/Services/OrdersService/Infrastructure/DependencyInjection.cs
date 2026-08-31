370dc629715437fd8c2f5cebd1484b0c42d8a912
Sara Maged <saramaged660@gmail.com>
2026-08-31 09:09:20 +0300

    remove unused AddDistributedMemoryCache registration

--- src/Services/OrdersService/Infrastructure/DependencyInjection.cs
+++ src/Services/OrdersService/Infrastructure/DependencyInjection.cs
@@ -85,10 +85,7 @@ namespace OrdersService.Infrastructure
 
             services.AddTransient<AuthorizationHeaderForwardingHandler>();
 
-            services.AddDistributedMemoryCache();
-
-            
-           
+        
 
             return services;
         }
