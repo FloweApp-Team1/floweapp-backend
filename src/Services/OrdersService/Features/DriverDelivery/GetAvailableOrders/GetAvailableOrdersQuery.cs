cc44e8d01a6939a030aaa2df5a708d5ed6a27785
Sara Maged <saramaged660@gmail.com>
2026-09-04 05:43:48 +0300

    implement Get Available Orders  For DriversHandler and Endpoint

---
+++ src/Services/OrdersService/Features/DriverDelivery/GetAvailableOrders/GetAvailableOrdersQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Shared.Requests;
+using Shared.Results;
+
+namespace OrdersService.Features.DriverDelivery.GetAvailableOrders
+{
+    public sealed record GetAvailableOrdersQuery(PaginationRequest Request)
+        : IRequest<Result<GetAvailableOrdersResponse>>;
+}
